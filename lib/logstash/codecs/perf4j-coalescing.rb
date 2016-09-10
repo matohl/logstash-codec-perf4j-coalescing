6389b58d0d013fb8a2e0b8f9c70873fed4fdf054
Mattias Ohlsson <mattias@Mattiass-MBP.lan>
2016-09-10 21:55:36 +0200

    Can now parse a block of coalesced performance information, outputting each metric as an own event prefixed with start and end times in iso8601 format

--- lib/logstash/codecs/perf4j-coalescing.rb
+++ lib/logstash/codecs/perf4j-coalescing.rb
@@ -33,9 +33,45 @@ class LogStash::Codecs::Perf4jCoalescing < LogStash::Codecs::Base
 
   public
   def decode(data)
+    #puts "data=" + data
     @lines.decode(data) do |line|
-      replace = { "message" => line["message"].to_s + @append }
-      yield LogStash::Event.new(replace)
+
+      message = line["message"]
+      #puts "length=" + message.length.to_s
+      if message.length == 0
+        #puts "end of line found"
+        #yield LogStash::Event.new( { "message" => @buffer.join(NL) } )
+
+        if @buffer.length > 2
+
+          # first get start and end time from first line
+          times = get_start_end_times(@buffer[0])
+          startTime = times[0]
+          endTime   = times[1]
+
+
+          # then scrap first and second line
+          @buffer.delete_at(1)
+          @buffer.delete_at(0)
+          @buffer.compact
+
+          @buffer.each { |line|  yield LogStash::Event.new( { "message" => startTime + " " + endTime + "    " +  line } ) }
+
+        end
+
+
+        buffer_reset
+      else
+        #puts "adding " + message
+        buffer_add(message)
+      end
+
+      #puts "line=" + line["message"]
+      #replace = { "message" => line["message"].to_s + @append }
+
+      #replace = { "message" => "hoj" }
+
+
     end
   end # def decode
 
@@ -43,5 +79,40 @@ class LogStash::Codecs::Perf4jCoalescing < LogStash::Codecs::Base
   def encode(event)
     @on_event.call(event, event["message"].to_s + @append + NL)
   end # def encode
+
+  def buffer_add(message)
+    (@buffer ||= []) << message
+  end
+
+  def buffer_reset
+    @buffer.clear
+  end
+
+  def get_start_end_times(line)
+    #2001-02-03T04:05+01:00
+
+    arr = line.split(" ")
+    date1 = arr[2] + "T" + arr[3]
+    date2 = arr[5] + "T" + arr[6]
+
+    # offset from UTC
+    offset_minutes = Time.now.getlocal.utc_offset / 60
+
+    if offset_minutes == 0
+      time_zone_suffix = "Z"
+    else
+      hours = offset_minutes / 60
+      minutes = offset_minutes % 60
+      time_zone_suffix = "+" + sprintf("%2.2d%2.2d", hours, minutes)
+    end
+    date1 << time_zone_suffix
+    date2 << time_zone_suffix
+
+    values = []
+    values.push(date1)
+    values.push(date2)
+
+    values
+  end
 
 end # class LogStash::Codecs::Perf4jCoalescing
--- spec/codecs/perf4j-coalescing_spec.rb
+++ spec/codecs/perf4j-coalescing_spec.rb
@@ -9,6 +9,7 @@ describe LogStash::Codecs::Perf4jCoalescing do
     LogStash::Codecs::Perf4jCoalescing.new
   end
 
+=begin
   it "should append a string to data" do
     data = "Test\n"
     subject.decode(data) do |event|
@@ -16,4 +17,58 @@ describe LogStash::Codecs::Perf4jCoalescing do
       expect(event["message"]).to eq("Test, Hello World!")
     end
   end
+=end
+
+  it "should parse perf4j block" do
+    data = "Performance Statistics   2016-09-01 01:50:00 - 2016-09-01 01:55:00\nTag                                                  Avg(ms)         Min         Max     Std Dev       Count\nAppService-getMessages                                 127.8          48         254        84.4           4\n\n"
+    subject.decode(data) do |event|
+      puts event["message"]
+      expect(true)
+    end
+  end
+
+=begin
+  it "should separate three blocks" do
+    data = "one\n\ntwo\ntwo\n\nthree\nthree\nthree\n\n"
+    subject.decode(data) do |event|
+      puts event["message"]
+      expect(true)
+    end
+    end
+
+  it "should" do
+    str = "Performance Statistics   2016-09-01 13:00:00 - 2016-09-01 13:05:00"
+    #str.scan("\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d") {|match| puts match}
+    #out = str.scan("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d")
+    #puts out
+    #puts "len" + out.length.to_s
+
+    arr = str.split(" ")
+    arr.each { |item| puts item  + "\n" }
+
+    date1 = arr[2] + " " + arr[3]
+    date2 = arr[5] + " " + arr[6]
+
+    puts "date1=" + date1
+    puts "date2=" + date2
+
+    puts "timezone is " + Time.now.getlocal.zone
+    puts "timezone offset is " + Time.now.getlocal.utc_offset.to_s
+    offset_minutes = Time.now.getlocal.utc_offset / 60
+    puts "minutes offset is " + offset_minutes.to_s
+    hours = offset_minutes / 60
+    minutes = offset_minutes % 60
+    puts "hours=" + hours.to_s + ", minutes=" + minutes.to_s
+
+    iso8601_offset = sprintf("%2.2d%2.2d", hours, minutes)
+    puts "offset=" + iso8601_offset
+
+
+
+
+
+    expect(true)
+  end
+=end
+
 end
