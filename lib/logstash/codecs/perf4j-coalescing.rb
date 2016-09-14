71db0569154201c03328169107f1d49d5533d3fd
Mattias Ohlsson <mattias@Mattiass-MBP.lan>
2016-09-14 21:33:19 +0200

    Rewritten to be based on the multiline codec, in order to take advantage of its flushing

--- lib/logstash/codecs/perf4j-coalescing.rb
+++ lib/logstash/codecs/perf4j-coalescing.rb
@@ -3,97 +3,287 @@ require "logstash/codecs/base"
 require "logstash/codecs/line"
 require "logstash/namespace"
 
-# Add any asciidoc formatted documentation here
-class LogStash::Codecs::Perf4jCoalescing < LogStash::Codecs::Base
+require "logstash/util/charset"
+require "logstash/timestamp"
+require "logstash/codecs/auto_flush"
 
-  # This example codec will append a string to the message field
-  # of an event, either in the decoding or encoding methods
+
+
+module LogStash module Codecs class Perf4jCoalescing < LogStash::Codecs::Base
+  config_name "perf4j-coalescing"
+
+  # This coalescing perf4j codec will BLA BLA TODO
   #
   # This is only intended to be used as an example.
   #
   # input {
   #   stdin { codec => perfj-coalescing }
   # }
-  #
-  # or
-  #
-  # output {
-  #   stdout { codec => perfj-coalescing }
-  # }
-  config_name "perf4j-coalescing"
 
-  # Append a string to the message
-  config :append, :validate => :string, :default => ', Hello World!'
+  # Logstash ships by default with a bunch of patterns, so you don't
+  # necessarily need to define this yourself unless you are adding additional
+  # patterns.
+  #
+  # Pattern files are plain text with format:
+  # [source,ruby]
+  #     NAME PATTERN
+  #
+  # For example:
+  # [source,ruby]
+  #     NUMBER \d+
+  config :patterns_dir, :validate => :array, :default => []
+
+  # The character encoding used in this input. Examples include `UTF-8`
+  # and `cp1252`
+  #
+  # This setting is useful if your log files are in `Latin-1` (aka `cp1252`)
+  # or in another character set other than `UTF-8`.
+  #
+  # This only affects "plain" format logs since JSON is `UTF-8` already.
+  config :charset, :validate => ::Encoding.name_list, :default => "UTF-8"
+
+  # The accumulation of events can make logstash exit with an out of memory error
+  # if event boundaries are not correctly defined. This settings make sure to flush
+  # multiline events after reaching a number of lines, it is used in combination
+  # max_bytes.
+  #config :max_lines, :validate => :number, :default => 500
+  config :max_lines, :validate => :number, :default => 5
+
+  # The accumulation of events can make logstash exit with an out of memory error
+  # if event boundaries are not correctly defined. This settings make sure to flush
+  # multiline events after reaching a number of bytes, it is used in combination
+  # max_lines.
+  config :max_bytes, :validate => :bytes, :default => "10 MiB"
+
+  # The accumulation of multiple lines will be converted to an event when either a
+  # matching new line is seen or there has been no new data appended for this time
+  # auto_flush_interval. No default.  If unset, no auto_flush. Units: seconds
+  config :auto_flush_interval, :validate => :number
 
   public
   def register
-    @lines = LogStash::Codecs::Line.new
-    @lines.charset = "UTF-8"
-  end
+    require "grok-pure" # rubygem 'jls-grok'
+    require 'logstash/patterns/core'
 
-  public
-  def decode(data)
-    #puts "data=" + data
-    @lines.decode(data) do |line|
+    # The regular expression to match.
+    @pattern=""
 
-      message = line["message"]
-      #puts "length=" + message.length.to_s
-      if message.length == 0
-        #puts "end of line found"
-        #yield LogStash::Event.new( { "message" => @buffer.join(NL) } )
+    # If the pattern matched, does event belong to the next or previous event?
+    @what =  "previous"
 
-        if @buffer.length > 2
+    # Negate the regexp pattern ('if not matched').
+    @negate = false
 
-          # first get start and end time from first line
-          times = get_start_end_times(@buffer[0])
-          startTime = times[0]
-          endTime   = times[1]
+    @auto_flush_interval = 5
 
 
-          # then scrap first and second line
-          @buffer.delete_at(1)
-          @buffer.delete_at(0)
-          @buffer.compact
+    # Detect if we are running from a jarfile, pick the right path.
+    patterns_path = []
+    patterns_path += [LogStash::Patterns::Core.path]
 
-          @buffer.each { |line|  yield LogStash::Event.new( { "message" => startTime + " " + endTime + "    " +  line } ) }
+    @grok = Grok.new
 
-        end
-
-
-        buffer_reset
-      else
-        #puts "adding " + message
-        buffer_add(message)
+    @patterns_dir = patterns_path.to_a + @patterns_dir
+    @patterns_dir.each do |path|
+      if ::File.directory?(path)
+        path = ::File.join(path, "*")
       end
 
-      #puts "line=" + line["message"]
-      #replace = { "message" => line["message"].to_s + @append }
+      Dir.glob(path).each do |file|
+        @logger.info("Grok loading patterns from file", :path => file)
+        @grok.add_patterns_from_file(file)
+      end
+    end
 
-      #replace = { "message" => "hoj" }
+    @grok.compile(@pattern)
+    @logger.debug("Registered Perf4jCoalescing plugin", :type => @type, :config => @config)
 
+    reset_buffer
 
+    @handler = method("do_#{@what}".to_sym)
+
+    @converter = LogStash::Util::Charset.new(@charset)
+    @converter.logger = @logger
+    if @auto_flush_interval
+      # will start on first decode
+      @auto_flush_runner = AutoFlush.new(self, @auto_flush_interval)
+    end
+  end # def register
+
+  def use_mapper_auto_flush
+    return unless auto_flush_active?
+    @auto_flush_runner = AutoFlushUnset.new(nil, nil)
+    @auto_flush_interval = @auto_flush_interval.to_f
+  end
+
+  def accept(listener)
+    # memoize references to listener that holds upstream state
+    @previous_listener = @last_seen_listener || listener
+    @last_seen_listener = listener
+    decode(listener.data) do |event|
+      what_based_listener.process_event(event)
+    end
+  end
+
+  def decode(text, &block)
+    text = @converter.convert(text)
+    text.split("\n").each do |line|
+      match = @grok.match(line)
+      @logger.debug("Perf4jCoalescing", :pattern => @pattern, :text => line,
+                    :match => !match.nil?, :negate => @negate)
+
+      # Add negate option
+      #match = (match and !@negate) || (!match and @negate)
+
+      if line.length == 0
+        match = true
+      else
+        match = false
+      end
+      @handler.call(line, match, &block)
     end
   end # def decode
 
-  public
-  def encode(event)
-    @on_event.call(event, event["message"].to_s + @append + NL)
-  end # def encode
-
-  def buffer_add(message)
-    (@buffer ||= []) << message
+  def buffer(text)
+    @buffer_bytes += text.bytesize
+    @buffer.push(text)
   end
 
-  def buffer_reset
-    @buffer.clear
+  def flush(&block)
+    @logger.info("Flush called")
+    if block_given? && @buffer.any?
+      no_error = true
+      events = merge_events
+      begin
+        #yield events
+        events.each  { |evt|
+          yield evt
+        }
+      rescue ::Exception => e
+        # need to rescue everything
+        # likliest cause: backpressure or timeout by exception
+        # can't really do anything but leave the data in the buffer for next time if there is one
+        @logger.error("Multiline: flush downstream error", :exception => e)
+        no_error = false
+      end
+      reset_buffer if no_error
+    end
+  end
+
+  def auto_flush(listener = @last_seen_listener)
+    return if listener.nil?
+
+    flush do |event|
+      listener.process_event(event)
+    end
+  end
+
+  def merge_events
+
+    # when we end up here, there should be an entire block of coalesced performance statistics in the buffer,
+    # where the first line will contain the time interval, the second is just headings (discard it), and from
+    # the third line there are real statistics
+    events = []
+    if @buffer.length > 2
+
+      # first get start and end time from first line
+      times = get_start_end_times(@buffer[0])
+      startTime = times[0]
+      endTime   = times[1]
+
+
+      # then scrap first and second line
+      @buffer.delete_at(1)
+      @buffer.delete_at(0)
+      @buffer.compact
+
+      @buffer.each { |line|
+
+        splitLine = line.split(" ")
+
+        events.push( LogStash::Event.new(
+                LogStash::Event::TIMESTAMP => startTime,
+                "message" => startTime + " " + endTime + "    " +  line,
+                "startTime" => startTime,
+                "endTime" => endTime,
+                "tag" => splitLine[0],
+                "average" => splitLine[1],
+                "min" => splitLine[2],
+                "max" => splitLine[3],
+                "StdDev" => splitLine[4],
+                "count" => splitLine[5]
+            ))
+      }
+
+    end
+
+    events
+  end
+
+  def reset_buffer
+    @buffer = []
+    @buffer_bytes = 0
+  end
+
+  def doing_previous?
+    @what == "previous"
+  end
+
+  def what_based_listener
+    doing_previous? ? @previous_listener : @last_seen_listener
+  end
+=begin
+  def do_next(text, matched, &block)
+    buffer(text)
+    auto_flush_runner.start
+    flush(&block) if !matched || buffer_over_limits?
+  end
+=end
+
+  def do_previous(text, matched, &block)
+    flush(&block) if matched #if !matched || buffer_over_limits?
+    auto_flush_runner.start
+    if !matched
+      buffer(text)
+      @logger.info("add to buffer:" + text)
+    end
+  end
+
+  def over_maximum_lines?
+    @buffer.size > @max_lines
+  end
+
+  def over_maximum_bytes?
+    @buffer_bytes >= @max_bytes
+  end
+
+  def buffer_over_limits?
+    over_maximum_lines? || over_maximum_bytes?
+  end
+
+  def encode(event)
+    # Nothing to do.
+    @on_event.call(event, event)
+  end # def encode
+
+  def close
+    auto_flush_runner.stop
+  end
+
+  def auto_flush_active?
+    !@auto_flush_interval.nil?
+  end
+
+  def auto_flush_runner
+    @auto_flush_runner || AutoFlushUnset.new(nil, nil)
   end
 
   def get_start_end_times(line)
     #2001-02-03T04:05+01:00
 
     arr = line.split(" ")
-    date1 = arr[2] + "T" + arr[3]
-    date2 = arr[5] + "T" + arr[6]
+    date1 = arr[2] + "T" + arr[3] + ".000"
+    date2 = arr[5] + "T" + arr[6] + ".000"
 
     # offset from UTC
     offset_minutes = Time.now.getlocal.utc_offset / 60
@@ -103,7 +293,7 @@ class LogStash::Codecs::Perf4jCoalescing < LogStash::Codecs::Base
     else
       hours = offset_minutes / 60
       minutes = offset_minutes % 60
-      time_zone_suffix = "+" + sprintf("%2.2d%2.2d", hours, minutes)
+      time_zone_suffix = "+" + sprintf("%2.2d:%2.2d", hours, minutes)
     end
     date1 << time_zone_suffix
     date2 << time_zone_suffix
@@ -115,4 +305,4 @@ class LogStash::Codecs::Perf4jCoalescing < LogStash::Codecs::Base
     values
   end
 
-end # class LogStash::Codecs::Perf4jCoalescing
+end end end # class LogStash::Codecs::Perf4jCoalescing
